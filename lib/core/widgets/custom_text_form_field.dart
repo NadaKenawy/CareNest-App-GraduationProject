6581e20e8d25d106632f670f53cf4f7974eefa7f
NadaKenawy <nadakenawy298@gmail.com>
2024-10-12 23:46:11 +0300

    add cuttom button and text form field

--- lib/core/widgets/custom_text_form_field.dart
+++ lib/core/widgets/custom_text_form_field.dart
@@ -1,10 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class CustomTextFormField extends StatelessWidget {
-  const CustomTextFormField({super.key});
+
+class AppTextFormField extends StatelessWidget {
+  const AppTextFormField({
+    super.key,
+    this.contentPadding,
+    this.focusedBorder,
+    this.enabledBorder,
+    this.inputTextStyle,
+    this.hintStyle,
+    required this.hintText,
+    this.obscureText,
+    this.suffixIcon,
+    this.backgoundColor,
+  });
+  final EdgeInsetsGeometry? contentPadding;
+  final InputBorder? focusedBorder;
+  final InputBorder? enabledBorder;
+  final TextStyle? inputTextStyle;
+  final TextStyle? hintStyle;
+  final String hintText;
+  final bool? obscureText;
+  final Widget? suffixIcon;
+  final Color? backgoundColor;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return TextFormField(
+      decoration: InputDecoration(
+        isDense: true,
+        contentPadding: contentPadding ??
+            EdgeInsets.symmetric(horizontal: 20.w, vertical: 18.h),
+        focusedBorder: focusedBorder ??
+            OutlineInputBorder(
+              borderRadius: BorderRadius.circular(16.r),
+              borderSide: BorderSide(
+             //   color: ColorsManager.mainBlue,
+                width: 1.3.w,
+              ),
+            ),
+        enabledBorder: enabledBorder ??
+            OutlineInputBorder(
+              borderRadius: BorderRadius.circular(16.r),
+              borderSide: BorderSide(
+              //  color: ColorsManager.lighterGray,
+                width: 1.3.w,
+              ),
+            ),
+      //  hintStyle: hintStyle ?? TextStyles.font14LightGrayRegular,
+        hintText: hintText,
+        suffixIcon: suffixIcon,
+       // fillColor: backgoundColor ?? ColorsManager.moreLightGray,
+        filled: true,
+      ),
+      obscureText: obscureText ?? false,
+      //style: inputTextStyle ?? TextStyles.font14DarkBlueMedium,
+    );
   }
 }
