69b31826590aa4ae1467a508fb96574f23cd82b7
MahmoudAladwy <mahmoudaladwy774@gmail.com>
2024-10-13 00:40:12 +0300

    - added api constants - added api error handler - added api error model - added api result - added api _service - added dio_factory

---
+++ lib/core/networking/api_constants.dart
@@ -0,0 +1,24 @@
+class ApiConstants {
+  static const String apiBaseUrl = "https://carenest-serverside.vercel.app/";
+  static const String login = "auth/login";
+  static const String signup = "auth/signup";
+}
+
+class ApiErrors {
+  static const String badRequestError = "Bad Request Error";
+  static const String noContent = "No Content";
+  static const String forbiddenError = "Forbidden Error";
+  static const String authenticationError = "Authentication Error";
+  static const String notFoundError = "Not Found Error";
+  static const String conflictError = "Conflict Error";
+  static const String internalServerError = "Internal Server Error";
+  static const String unknownError = "Unknown Error";
+  static const String timeoutError = "Timeout Error";
+  static const String defaultError = "Default Error";
+  static const String cacheError = "Cache Error";
+  static const String noInternetError = "No Internet Error";
+  static const String loadingMessage = "Loading_Message";
+  static const String retryAgainMessage = "Retry_Again_ Message";
+  static const String unauthorizedError = "Unauthorized Error";
+  static const String ok = "OK";
+}
---
+++ lib/core/networking/api_service.dart
@@ -0,0 +1,10 @@
+import 'package:care_nest/core/networking/api_constants.dart';
+import 'package:dio/dio.dart';
+import 'package:retrofit/error_logger.dart';
+import 'package:retrofit/http.dart';
+part 'api_service.g.dart';
+
+@RestApi(baseUrl: ApiConstants.apiBaseUrl)
+abstract class ApiService {
+  factory ApiService(Dio dio, {String baseUrl}) = _ApiService;
+}
