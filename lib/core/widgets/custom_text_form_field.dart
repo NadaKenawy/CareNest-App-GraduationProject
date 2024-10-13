620ea02970e47f1cb98c41e494a5335d7f655e5f
NadaKenawy <nadakenawy298@gmail.com>
2024-10-13 03:52:09 +0300

    Refactor CustomTextFormField and SignUpBody Widgets

--- lib/core/widgets/custom_text_form_field.dart
+++ lib/core/widgets/custom_text_form_field.dart
@@ -1,6 +1,6 @@
+import 'package:care_nest/core/utils/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 
 class AppTextFormField extends StatelessWidget {
   const AppTextFormField({
@@ -13,8 +13,10 @@ class AppTextFormField extends StatelessWidget {
     required this.hintText,
     this.obscureText,
     this.suffixIcon,
-    this.backgoundColor,
+    this.backgroundColor,
+    this.width,
   });
+
   final EdgeInsetsGeometry? contentPadding;
   final InputBorder? focusedBorder;
   final InputBorder? enabledBorder;
@@ -23,39 +25,47 @@ class AppTextFormField extends StatelessWidget {
   final String hintText;
   final bool? obscureText;
   final Widget? suffixIcon;
-  final Color? backgoundColor;
+  final Color? backgroundColor;
+  final double? width;
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      decoration: InputDecoration(
-        isDense: true,
-        contentPadding: contentPadding ??
-            EdgeInsets.symmetric(horizontal: 20.w, vertical: 18.h),
-        focusedBorder: focusedBorder ??
-            OutlineInputBorder(
-              borderRadius: BorderRadius.circular(16.r),
-              borderSide: BorderSide(
-             //   color: ColorsManager.mainBlue,
-                width: 1.3.w,
+    return SizedBox(
+      width: width ?? double.infinity,
+      child: TextFormField(
+        decoration: InputDecoration(
+          isDense: true,
+          contentPadding: contentPadding ??
+              EdgeInsets.symmetric(horizontal: 20.w, vertical: 18.h),
+          focusedBorder: focusedBorder ??
+              OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20.r),
+                borderSide: BorderSide(
+                  color: Colors.blue,
+                  width: 1.3.w,
+                ),
               ),
-            ),
-        enabledBorder: enabledBorder ??
-            OutlineInputBorder(
-              borderRadius: BorderRadius.circular(16.r),
-              borderSide: BorderSide(
-              //  color: ColorsManager.lighterGray,
-                width: 1.3.w,
+          enabledBorder: enabledBorder ??
+              OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20.r),
+                borderSide: BorderSide(
+                  color: ColorsData.primaryBlueColor,
+                  width: 2.w,
+                ),
               ),
-            ),
-      //  hintStyle: hintStyle ?? TextStyles.font14LightGrayRegular,
-        hintText: hintText,
-        suffixIcon: suffixIcon,
-       // fillColor: backgoundColor ?? ColorsManager.moreLightGray,
-        filled: true,
+          hintText: hintText,
+          hintStyle:
+              hintStyle ?? const TextStyle(color: ColorsData.primaryBlueColor),
+          suffixIcon: SizedBox(
+            width: 24.w,
+            height: 24.h,
+            child: suffixIcon,
+          ),
+          filled: true,
+          fillColor: Colors.transparent,
+        ),
+        obscureText: obscureText ?? false,
       ),
-      obscureText: obscureText ?? false,
-      //style: inputTextStyle ?? TextStyles.font14DarkBlueMedium,
     );
   }
 }
--- lib/features/sign_up/ui/widgets/sign_up_body.dart
+++ lib/features/sign_up/ui/widgets/sign_up_body.dart
@@ -1,49 +1,114 @@
+import 'package:care_nest/core/widgets/custom_text_form_field.dart';
 import 'package:care_nest/features/sign_up/ui/widgets/sign_up_image.dart';
 import 'package:care_nest/features/sign_up/ui/widgets/sign_up_title.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../core/utils/colors.dart';
 
-class SignUpBody extends StatelessWidget {
+class SignUpBody extends StatefulWidget {
   const SignUpBody({super.key});
+
+  @override
+  State<SignUpBody> createState() => _SignUpBodyState();
+}
+
+class _SignUpBodyState extends State<SignUpBody> {
+  bool _isPasswordVisible = false;
+  bool _isConfirmPasswordVisible = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Stack(
-              children: [
-                const SignUpImage(),
-                Positioned(
-                  top: 370,
-                  left: 0,
-                  right: 0,
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(vertical: 24),
-                    decoration: const BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(70),
-                        topRight: Radius.circular(70),
-                      ),
-                    ),
-                    child:  const Column(
-                      children: [
-                        SignUpTitle(),
-                        SizedBox(height: 20),
-                      ],
-                    ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Stack(
+            clipBehavior: Clip.none,
+            children: [
+              const SignUpImage(),
+              Container(
+                margin: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.46),
+                width: double.infinity,
+                padding:
+                    const EdgeInsets.symmetric(vertical: 24, horizontal: 16),
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(70),
+                    topRight: Radius.circular(70),
                   ),
                 ),
-              ],
-            ),
+                child: Column(
+                  children: [
+                    const SignUpTitle(),
+                    const SizedBox(height: 24),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
+                          child: AppTextFormField(
+                            hintText: "First Name",
+                            width: MediaQuery.of(context).size.width * 0.4,
+                          ),
+                        ),
+                        const SizedBox(width: 16),
+                        Expanded(
+                          child: AppTextFormField(
+                            hintText: "Last Name",
+                            width: MediaQuery.of(context).size.width * 0.4,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 16),
+                    const AppTextFormField(
+                      hintText: "Email",
+                    ),
+                    const SizedBox(height: 16),
+                    AppTextFormField(
+                      hintText: "Password",
+                      obscureText: !_isPasswordVisible,
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          _isPasswordVisible
+                              ? Icons.visibility_off
+                              : Icons.visibility,
+                          color: ColorsData.primaryBlueColor,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _isPasswordVisible = !_isPasswordVisible;
+                          });
+                        },
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    AppTextFormField(
+                      hintText: "Confirm Password",
+                      obscureText: !_isConfirmPasswordVisible,
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          _isConfirmPasswordVisible
+                              ? Icons.visibility_off
+                              : Icons.visibility,
+                          color: ColorsData.primaryBlueColor,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _isConfirmPasswordVisible =
+                                !_isConfirmPasswordVisible;
+                          });
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
 }
-
-
