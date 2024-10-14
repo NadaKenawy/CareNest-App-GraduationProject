1099a98404314c05e47ca7a4a71af6d22c84bf4a
MahmoudAladwy <mahmoudaladwy774@gmail.com>
2024-10-14 04:05:40 +0300

    -added login cubit & states

--- lib/core/di/service_locator.dart
+++ lib/core/di/service_locator.dart
@@ -1,11 +1,25 @@
-import 'package:care_nest/core/utils/api_service.dart';
+import 'package:care_nest/core/networking/api_service.dart';
+import 'package:care_nest/features/login/data/repos/login_repo.dart';
+import 'package:care_nest/features/login/logic/login_cubit/login_cubit.dart';
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 
 final getIt = GetIt.instance;
 
 void setupServiceLocator() {
-  getIt.registerSingleton<ApiService>(ApiService(
-    Dio(),
-  ));
+  getIt.registerSingleton<ApiService>(
+    ApiService(
+      Dio(),
+    ),
+  );
+  getIt.registerSingleton<LoginRepo>(
+    LoginRepo(
+      getIt<ApiService>(),
+    ),
+  );
+  getIt.registerSingleton<LoginCubit>(
+    LoginCubit(
+      getIt<LoginRepo>(),
+    ),
+  );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:care_nest/core/di/service_locator.dart';
 import 'package:care_nest/core/routing/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  setupServiceLocator();
   runApp(const CareNest());
 }
 
