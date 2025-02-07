61b1abf2222ecc5ed944304e7abfae40b3159472
NadaKenawy <nadakenawy298@gmail.com>
2025-02-07 08:24:41 +0200

    get babies in dropdown

--- lib/features/baby_growth/ui/widgets/growth_info_card.dart
+++ lib/features/baby_growth/ui/widgets/growth_info_card.dart
@@ -9,72 +9,54 @@ class GrowthInfoCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Positioned(
-      top: 180.h,
-      left: 0,
-      right: 0,
-      child: Container(
-        height: 140.h,
-        decoration: const BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(20),
-            topRight: Radius.circular(20),
-          ),
-          border: Border(
-            bottom: BorderSide(color: Colors.grey, width: 1),
-          ),
-        ),
-        child: Padding(
-          padding: EdgeInsets.all(16.w),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16.w),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Text(
-                    'Last recorded weight',
-                    style: TextStyle(
-                        fontSize: 12.sp,
-                        color: Colors.grey,
-                        fontWeight: FontWeightHelper.semiBold),
-                  ),
-                  Text(
-                    '  6.4 kg',
-                    style: TextStyle(
-                        fontSize: 12.sp, fontWeight: FontWeightHelper.semiBold),
-                  )
-                ],
-              ),
-              SizedBox(height: 12.h),
-              Row(
-                children: [
-                  Text(
-                    'Your baby’s current weight',
-                    style: TextStyle(
-                        fontSize: 20.sp, fontWeight: FontWeightHelper.semiBold),
-                  ),
-                  Text(
-                    '  7.1 kg',
-                    style: TextStyle(
-                        fontSize: 20.sp,
-                        color: const Color(0xff3F7726),
-                        fontWeight: FontWeightHelper.semiBold),
-                  )
-                ],
-              ),
-              SizedBox(height: 4.h),
               Text(
-                'Your baby is growing well!',
+                'Last recorded weight',
+                style: TextStyle(
+                    fontSize: 12.sp,
+                    color: Colors.grey,
+                    fontWeight: FontWeightHelper.semiBold),
+              ),
+              Text(
+                '  6.4 kg',
+                style: TextStyle(
+                    fontSize: 12.sp, fontWeight: FontWeightHelper.semiBold),
+              )
+            ],
+          ),
+          SizedBox(height: 12.h),
+          Row(
+            children: [
+              Text(
+                'Your baby’s current weight',
+                style: TextStyle(
+                    fontSize: 20.sp, fontWeight: FontWeightHelper.semiBold),
+              ),
+              Text(
+                '  7.1 kg',
                 style: TextStyle(
                     fontSize: 20.sp,
                     color: const Color(0xff3F7726),
                     fontWeight: FontWeightHelper.semiBold),
-              ),
+              )
             ],
           ),
-        ),
+          SizedBox(height: 4.h),
+          Text(
+            'Your baby is growing well!',
+            style: TextStyle(
+                fontSize: 20.sp,
+                color: const Color(0xff3F7726),
+                fontWeight: FontWeightHelper.semiBold),
+          ),
+        ],
       ),
     );
   }
--- lib/features/baby_growth/ui/widgets/header_section.dart
+++ lib/features/baby_growth/ui/widgets/header_section.dart
@@ -1,20 +1,16 @@
 import 'package:care_nest/features/baby_growth/ui/widgets/babies_dropdown.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:go_router/go_router.dart';
-import 'package:care_nest/core/theme/font_weight_helper.dart';
 
 class HeaderSection extends StatelessWidget {
   final String selectedBaby;
   final String selectedImage;
-  final List<Map<String, String>> babies;
   final Function(String, String) onBabySelected;
 
   const HeaderSection({
     super.key,
     required this.selectedBaby,
     required this.selectedImage,
-    required this.babies,
     required this.onBabySelected,
   });
 
@@ -33,21 +29,20 @@ class HeaderSection extends StatelessWidget {
           children: [
             IconButton(
               icon: const Icon(Icons.arrow_back, color: Colors.black),
-              onPressed: () => GoRouter.of(context).pop(),
+              onPressed: () => Navigator.pop(context),
             ),
             SizedBox(width: 8.w),
             Text(
-              'Watch $selectedBaby\'s\nAmazing Growth!',
+              'Watch ${selectedBaby[0].toUpperCase()}${selectedBaby.substring(1)}\'s\nAmazing Growth!',
               style: TextStyle(
                 color: Colors.black,
                 fontSize: 24.sp,
-                fontWeight: FontWeightHelper.semiBold,
+                fontWeight: FontWeight.w600,
               ),
             ),
             const Spacer(),
             BabiesDropdown(
               selectedImage: selectedImage,
-              babies: babies,
               onBabySelected: onBabySelected,
             ),
           ],
