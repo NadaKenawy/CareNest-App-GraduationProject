e3c434190ca509cf15af154f8fbe787b291be042
MahmoudAladwy <mahmoudaladwy774@gmail.com>
2025-03-06 15:21:21 +0200

    - remove hive

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,11 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:hive_flutter/adapters.dart';
 import 'carenest_app.dart';
 import 'simple_bloc_observer.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Hive.initFlutter();
   await Firebase.initializeApp();
   Bloc.observer = SimpleBlocObserver();
   setupGetIt();
