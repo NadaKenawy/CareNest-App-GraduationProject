107458c616abf4a4133f68b97ea6ce34c665b7cc
NadaKenawy <nadakenawy298@gmail.com>
2025-03-11 02:24:43 +0200

    add geolocator to bring current location.

--- lib/features/doctors/ui/doctors_screen.dart
+++ lib/features/doctors/ui/doctors_screen.dart
@@ -1,8 +1,55 @@
-import 'package:care_nest/features/doctors/ui/widgets/doctors_screen_body.dart';
-import 'package:flutter/material.dart';
+import 'dart:developer';
 
-class DoctorsScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:care_nest/features/doctors/ui/widgets/doctors_screen_body.dart';
+
+class DoctorsScreen extends StatefulWidget {
   const DoctorsScreen({super.key});
+
+  @override
+  _DoctorsScreenState createState() => _DoctorsScreenState();
+}
+
+class _DoctorsScreenState extends State<DoctorsScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _requestLocation();
+  }
+
+  Future<void> _requestLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      log('Location service is disabled');
+      return;
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        log('Location permission denied');
+        return;
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      log('Location permission permanently denied');
+      return;
+    }
+
+    Position position = await GeolocatorPlatform.instance.getCurrentPosition(
+      locationSettings: const LocationSettings(
+        accuracy: LocationAccuracy.high,
+      ),
+    );
+
+    log('Current location: ${position.latitude}, ${position.longitude}');
+  }
 
   @override
   Widget build(BuildContext context) {
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -12,6 +12,7 @@ import firebase_messaging
 import flutter_inappwebview_macos
 import flutter_local_notifications
 import flutter_secure_storage_macos
+import geolocator_apple
 import just_audio
 import path_provider_foundation
 import record_darwin
@@ -28,6 +29,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   RecordPlugin.register(with: registry.registrar(forPlugin: "RecordPlugin"))
