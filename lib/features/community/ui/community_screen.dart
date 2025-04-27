c52f18117bd6c9d9eea6a0fdacd4e689c3dde9c2
NadaKenawy <nadakenawy298@gmail.com>
2025-04-27 19:43:56 +0300

    community is done

--- lib/features/community/ui/community_screen.dart
+++ lib/features/community/ui/community_screen.dart
@@ -1,11 +1,34 @@
-import 'package:care_nest/features/community/ui/widgets/community_screen_body.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
+import '../logic/create_message/create_message_cubit.dart';
+import '../logic/get_community_messages/get_community_messages_cubit.dart';
+import '../data/repos/create_message_repo.dart';
+import 'widgets/community_screen_body.dart';
 
-class CommunityScreen extends StatelessWidget {
+class CommunityScreen extends StatefulWidget {
   const CommunityScreen({super.key});
 
   @override
+  State<CommunityScreen> createState() => _CommunityScreenState();
+}
+
+class _CommunityScreenState extends State<CommunityScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const CommunityScreenBody();
+    return Scaffold(
+      body: BlocProvider(
+        create: (context) => CreateMessageCubit(
+          GetIt.instance<CreateMessageRepo>(),
+          context.read<GetCommunityMessagesCubit>(),
+        ),
+        child: const CommunityScreenBody(),
+      ),
+    );
   }
 }
