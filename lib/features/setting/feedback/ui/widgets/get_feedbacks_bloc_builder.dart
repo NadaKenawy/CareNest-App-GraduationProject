9127031d00f313d0d450be2d452d1d5f90e1f97e
NadaKenawy <nadakenawy298@gmail.com>
2025-06-25 08:33:37 +0300

    add feedback skeleton

--- lib/features/setting/feedback/ui/widgets/get_feedbacks_bloc_builder.dart
+++ lib/features/setting/feedback/ui/widgets/get_feedbacks_bloc_builder.dart
@@ -1,10 +1,12 @@
-import 'package:care_nest/core/theme/colors_manager.dart';
 import 'package:care_nest/features/setting/feedback/logic/get_feedback_cubit/get_feedbacks_cubit.dart';
 import 'package:care_nest/features/setting/feedback/ui/widgets/feedback_screen_body.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../data/models/get_feedback/get_feedbacks_response.dart';
 import '../../logic/get_feedback_cubit/get_feedbacks_state.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:care_nest/features/setting/feedback/ui/widgets/feedback_header.dart';
+import 'package:care_nest/features/setting/ui/widgets/setting_app_bar.dart';
 
 class GetFeedbacksBlocBuilder extends StatelessWidget {
   const GetFeedbacksBlocBuilder({
@@ -22,8 +24,6 @@ class GetFeedbacksBlocBuilder extends StatelessWidget {
             return setupLoading();
           },
           success: (getFeedbacksResponse) {
-           
-
             return setupSuccess(getFeedbacksResponse);
           },
           error: (error) {
@@ -36,22 +36,149 @@ class GetFeedbacksBlocBuilder extends StatelessWidget {
   }
 
   Widget setupLoading() {
-    return const Center(
-      child: CircularProgressIndicator(
-        color: ColorsManager.primaryBlueColor,
-        strokeWidth: 2.0,
-      ),
+    return const Column(
+      children: [
+        SettingAppBar(title: 'Feedbacks', showBackButton: true),
+        FeedbackScreenBodySkeleton(
+          avgRating: '0.0',
+          totalReviews: 0,
+        ),
+      ],
     );
   }
 
-  Widget setupSuccess( GetFeedbacksResponse getFeedbacksResponse) {
+  Widget setupSuccess(GetFeedbacksResponse getFeedbacksResponse) {
     return FeedbackScreenBody(
       getFeedbacksResponse: getFeedbacksResponse,
-      
     );
   }
 
   Widget setupError() {
     return const SizedBox.shrink();
+  }
+}
+
+class ReviewCardSkeleton extends StatelessWidget {
+  const ReviewCardSkeleton({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          children: [
+            Container(
+              width: 44.w,
+              height: 44.w,
+              decoration: BoxDecoration(
+                color: Colors.grey[300],
+                shape: BoxShape.circle,
+              ),
+            ),
+            SizedBox(width: 12.w),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    width: 100.w,
+                    height: 16.h,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[300],
+                      borderRadius: BorderRadius.circular(8.r),
+                    ),
+                  ),
+                  SizedBox(height: 4.h),
+                  Row(
+                    children: List.generate(
+                      5,
+                      (index) => Container(
+                        width: 18.w,
+                        height: 18.w,
+                        margin: EdgeInsets.symmetric(horizontal: 1.w),
+                        decoration: BoxDecoration(
+                          color: Colors.grey[300],
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              width: 60.w,
+              height: 14.h,
+              decoration: BoxDecoration(
+                color: Colors.grey[300],
+                borderRadius: BorderRadius.circular(8.r),
+              ),
+            ),
+          ],
+        ),
+        SizedBox(height: 8.h),
+        Container(
+          width: double.infinity,
+          height: 16.h,
+          decoration: BoxDecoration(
+            color: Colors.grey[300],
+            borderRadius: BorderRadius.circular(8.r),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class FeedbacksListViewSkeleton extends StatelessWidget {
+  const FeedbacksListViewSkeleton({super.key, this.itemCount = 4});
+  final int itemCount;
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      physics: const NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      itemCount: itemCount,
+      separatorBuilder: (_, __) => SizedBox(height: 32.h),
+      itemBuilder: (context, index) => const ReviewCardSkeleton(),
+    );
+  }
+}
+
+class FeedbackScreenBodySkeleton extends StatelessWidget {
+  const FeedbackScreenBodySkeleton({
+    super.key,
+    required this.avgRating,
+    required this.totalReviews,
+  });
+  final String avgRating;
+  final int totalReviews;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            children: [
+              const SizedBox(height: 8),
+              FeedbackHeader(avgRating: avgRating, totalReviews: totalReviews),
+              const FeedbacksListViewSkeleton(),
+              const SizedBox(height: 12),
+              Container(
+                width: double.infinity,
+                height: 48.h,
+                decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  borderRadius: BorderRadius.circular(12.r),
+                ),
+              ),
+              const SizedBox(height: 16),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
